6adf25c2e781d2ff975ce322ccee8ee1fa6b4a74
Mangalarapu <ramu.mangalarapu1622@gmail.com>
2020-01-18 22:22:54 +0530

    Intro handling DB connection in Golang

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "user:Password@/db_name")
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+	defer db.Close()
+
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	fmt.Println("Connected successfully :)")
+}
